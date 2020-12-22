0d9bcf1f3ede8b6612905ecef6de5d880df8e8ff
Daniel Fang <danielfang@hotmail.com>
2020-12-22 12:51:17 -0800

    completed Xylophone app, will all sounds and visual feedback opacity

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        // when a button is pressed
+        sender.alpha = 0.5
+
+          //Code should execute after 0.2 second delay.
+          DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              sender.alpha = 1.0
+          }
+        playSound(sender.currentTitle!)
+    }
+    
+    func playSound(_ note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
     
 
 }
